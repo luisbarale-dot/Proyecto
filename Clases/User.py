621dba59a745522cbcbaba464957d691c2c94e87
luisbarale-dot <luis.barale@docente.ceibal.edu.uy>
2026-09-03 19:54:31 -0300

    corregir

---
+++ Clases/User.py
@@ -0,0 +1,22 @@
+class User:
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
+class Estudiante(user):
+    def __init__(self, nombre, apellido, calificaciones, asistencia = True):
+        super().__init__(nombre, apellido)
+        self.calificaciones = calificaciones
+        self.asistencia = asistencia
+
+class Tutor(user):
+    def __init__(self, nombre, apellido):
+        super().__init__(nombre, apellido)
+        self.registro_asistencia = True
+
+    def Asistencia(self):
+        if Estudiante.asistencia == True:
+            return self.registro_asistencia
+        else:
+            return self.registro_asistencia = False
+
