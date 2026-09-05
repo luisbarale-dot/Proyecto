5272e4887b2afb762594697a998c6242c801b8d3
Gianna Giupponi <gianna30stm@gmail.com>
2026-09-05 15:40:09 -0300

    Change calificaciones from int to list for history

--- Clases/clase_usuario.py
+++ Clases/clase_usuario.py
@@ -9,7 +9,7 @@ class Estudiante(Usuario):
         super().__init__(nombre, apellido, cedula, especialidad)
         self.año = anio
         self.asistencia = 0 #luego se usará para contar asistencia
-        self.calificaciones = 0 #idem
+        self.calificaciones = [] #idem (Entiendo que debería ser una lista, ya que según la consigna dice que tiene que registar el historial de las visitas 
 class Adscriptor(Usuario):
     def __init__(self, nombre, apellido, cedula, especialidad):
         super().__init__(nombre, apellido, cedula, especialidad)
