85b50dace83b3ee721ddea1645604ff7cd989489
luisbarale-dot <luis.barale@docente.ceibal.edu.uy>
2026-09-05 14:53:32 -0300

    REVISAR

---
+++ Clases/clase_tutor.py
@@ -0,0 +1,10 @@
+from clase_usuario import Tutor
+
+def visita(self):
+    pass #Se registrarán las visitas que haga.
+
+def calificar(self):
+    pass #Registro de calificaciones
+
+def juicio(self):
+    pass #Se agregará un juicio.
--- Clases/clase_usuario.py
+++ Clases/clase_usuario.py
@@ -4,23 +4,17 @@ class Usuario:
         self.apellido = apellido
         self.cedula = cedula
         self.especialidad = especialidad
-
 class Estudiante(Usuario):
     def __init__(self, nombre, apellido, cedula, especialidad, anio):
         super().__init__(nombre, apellido, cedula, especialidad)
         self.año = anio
         self.asistencia = 0 #luego se usará para contar asistencia
         self.calificaciones = 0 #idem
-
 class Adscriptor(Usuario):
     def __init__(self, nombre, apellido, cedula, especialidad):
         super().__init__(nombre, apellido, cedula, especialidad)
         self.__disponible = True    #Para corroborar la disponibilidad.
-
 class Tutor(Usuario):   #Se entiende por "Tutor" el docente de didáctica.
     def __init__(self, nombre, apellido, cedula, especialidad, anio):
         super().__init__(nombre, apellido, cedula, especialidad)
-        self.anno = anio 
-
-    def visita_practica(self):
-        pass
+        self.anno = anio
