7e0f432e31be59b2768068365d79d59d87ea5277
luisbarale-dot <luis.barale@docente.ceibal.edu.uy>
2026-09-07 14:47:02 -0300

    Clases (nuevos atributos)

--- Clases/adscriptores.py
+++ Clases/adscriptores.py
@@ -1,11 +1,13 @@
 from clase_usuario import Adscriptor
 
-def es_disponible(self):
+def esta_disponible(self):
     return self.__disponible   #Debe consultarse la disponibilidad del adscriptor
+
+def cambiar_disponibilidad(self, estado): #Cambiar el estado del prof adscriptor 
+    self.__disponible = estado
 
 def asistencia_practicante(self):
     pass #debe registrarse el control de asistencia del estudiante.
 
-def habilitar(self):
-    if self.__disponible == False:
-        pass #solicitar_adscriptor próximamente
+
+
--- Clases/clase_usuario.py
+++ Clases/clase_usuario.py
@@ -3,8 +3,6 @@ class Usuario:
         self.nombre = nombre
         self.apellido = apellido
         self.cedula = cedula
-
-
 class Estudiante(Usuario):
     def __init__(self, nombre, apellido, cedula, especialidad, fecha_de_nacimiento, direccion,centro_educativo_asiste, celular, anio):
         super().__init__(nombre, apellido, cedula)
@@ -14,34 +12,18 @@ class Estudiante(Usuario):
         self.direccion = direccion
         self.centro_educativo_asiste = centro_educativo_asiste
         self.celular = celular
-        
         self.asistencia = 0 #luego se usará para contar asistencia
         self.calificaciones = [] #idem (Entiendo que debería ser una lista, ya que según la consigna dice que tiene que registar el historial de las visitas y..hay que calcular promedio? para saber si cumple las que son necesarias
         self.visitas = [] #Para registrar el historial de visitas del estudiante
-
 class Adscriptor(Usuario):
-
     def __init__(self, nombre, apellido, cedula):
         super().__init__(nombre, apellido, cedula)
-        
         # Acá podría ir un método que registre las visitas del estudiante para saber que cumpla el mínimo de visitas necesarias
-
         self.__disponible = True #Para corroborar la disponibilidad.
-
-    def esta_disponible(self):
-        return self.__disponible
-
-    def cambiar_disponibilidad(self, estado): #Cambiar el estado del prof adscriptor 
-        self.__disponible = estado
-
-
 class Tutor(Usuario): #Se entiende por "Tutor" el docente de didáctica.
     def __init__(self, nombre, apellido, cedula, anio, materia_que_dicta):
         super().__init__(nombre, apellido, cedula)
         self.anio = anio
-        
-
-
 class CentroEducativo:
     def __init__(self, nombre, localidad, tipo, lista_de_adscriptores):
         self.nombre = nombre
@@ -57,4 +39,3 @@ class Visita:
         self.fecha = fecha
         self.nota = nota
         self.observaciones = observaciones
-
