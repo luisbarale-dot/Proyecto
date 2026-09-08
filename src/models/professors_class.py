6c3dab95f4233114e6bdfd4dc2d9336c4421b19a
Konnor Lovegood <himynameiskonnor@gmail.com>
2026-09-08 02:06:56 -0300

    Students, professors and users Class, delete userts

---
+++ src/models/professors_class.py
@@ -0,0 +1,9 @@
+class Professors():
+    def __init__(self, user_id, dni, name, second_name, last_name, second_last_name, course):
+        self.user_id = user_id
+        self.dni = dni
+        self.name = name
+        self.second_name = second_name
+        self.last_name = last_name
+        self.second_last_name = second_last_name
+        self.course = course
---
+++ src/models/students_class.py
@@ -0,0 +1,10 @@
+class Students():
+    def __init__(self, user_id, dni, name, second_name, last_name, second_last_name, course, grade):
+        self.user_id = user_id
+        self.dni = dni
+        self.name = name
+        self.second_name = second_name
+        self.last_name = last_name
+        self.second_last_name = second_last_name
+        self.course = course
+        self.grade = grade
