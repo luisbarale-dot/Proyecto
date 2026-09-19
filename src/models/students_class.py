777b9218ccb81852b6e63741a6a3362cfd359b7e
Gianna Giupponi <gianna30stm@gmail.com>
2026-09-19 17:37:40 -0300

    Update students_class.py

--- src/models/students_class.py
+++ src/models/students_class.py
@@ -1,10 +1,20 @@
 class Students():
-    def __init__(self, user_id, dni, name, second_name, last_name, second_last_name, course, grade):
+    def __init__(self, user_id, ci, name, second_name, last_name, second_last_name, course, grade, gender, bday, city, address, phone, email, educational_center, registration_card, specialization, reference_center):
         self.user_id = user_id
-        self.dni = dni
+        self.ci = ci
         self.name = name
         self.second_name = second_name
         self.last_name = last_name
         self.second_last_name = second_last_name
         self.course = course
         self.grade = grade
+        self.gender= gender
+        self.bday = bday
+        self.city = city
+        self.address = address
+        self.phone = phone
+        self.email = email
+        self.registration_card = registration_card
+        self.educational_center = educational_center
+        self.specialization = specialization
+        self.reference_center = reference_center
