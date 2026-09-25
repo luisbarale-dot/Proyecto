4893213cdf3cbeb634f4e80042541df2c65d588f
Gianna Giupponi <gianna30stm@gmail.com>
2026-09-24 23:59:45 -0300

    Refactor Students class to use private attributes
    Refactored Students class to use private attributes for better encapsulation.

--- src/models/students_class.py
+++ src/models/students_class.py
@@ -1,20 +1,24 @@
-class Students():
-    def __init__(self, user_id, ci, name, second_name, last_name, second_last_name, course, grade, gender, bday, city, address, phone, email, educational_center, registration_card, specialization, reference_center):
-        self.user_id = user_id
-        self.ci = ci
-        self.name = name
-        self.second_name = second_name
-        self.last_name = last_name
-        self.second_last_name = second_last_name
-        self.course = course
-        self.grade = grade
-        self.gender= gender
-        self.bday = bday
-        self.city = city
-        self.address = address
-        self.phone = phone
-        self.email = email
-        self.registration_card = registration_card
-        self.educational_center = educational_center
-        self.specialization = specialization
-        self.reference_center = reference_center
+class Students:
+    def __init__(self, user_id, ci, name, second_name, last_name, second_last_name,
+                 course, grade, gender, bday, city, address, phone, email,
+                 educational_center, registration_card, specialization,
+                 reference_center):
+                     
+        self.__user_id = user_id
+        self.__ci = ci
+        self.__name = name
+        self.__second_name = second_name
+        self.__last_name = last_name
+        self.__second_last_name = second_last_name
+        self.__course = course
+        self.__grade = grade
+        self.__gender = gender
+        self.__bday = bday
+        self.__city = city
+        self.__address = address
+        self.__phone = phone
+        self.__email = email
+        self.__educational_center = educational_center
+        self.__registration_card = registration_card
+        self.__specialization = specialization
+        self.__reference_center = reference_center
